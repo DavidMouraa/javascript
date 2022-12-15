310c4307feb914bbcf77a74d4824a3fce9f97b08
David Moura <119620606+DavidMouraa@users.noreply.github.com>
2022-12-15 18:44:11 -0300

    Atualizei o ex001

--- exercicios/ex001/javascript/scripts.js
+++ exercicios/ex001/javascript/scripts.js
@@ -1,24 +1,27 @@
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-
-var p = document.querySelector('#hours');
-var img = document.querySelector('#img-time');
-
-p.innerHTML = `Agora são ${hours}:${minutes.toString().padStart(2, '0')} horas`;
-
-if (hours < 12)
+function carregar()
 {
-    img.src = 'imagens/time-morning.jpg';
-    document.body.style.backgroundColor = '#f0ab5c';
-}
-else if (hours < 18)
-{
-    img.src = 'imagens/time-afternoon.jpg';
-    document.body.style.backgroundColor = '#ecd137';
-}
-else
-{
-    img.src = 'imagens/time-night.jpg';
-    document.body.style.backgroundColor = '#3d3921';
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    var p = document.querySelector('#hours');
+    var img = document.querySelector('#img-time');
+
+    p.innerHTML = `Agora são ${hours}:${minutes.toString().padStart(2, '0')} horas`;
+
+    if (hours < 12)
+    {
+        img.src = 'imagens/time-morning.jpg';
+        document.body.style.backgroundColor = '#f0ab5c';
+    }
+    else if (hours < 18)
+    {
+        img.src = 'imagens/time-afternoon.jpg';
+        document.body.style.backgroundColor = '#ecd137';
+    }
+    else
+    {
+        img.src = 'imagens/time-night.jpg';
+        document.body.style.backgroundColor = '#212d3d';
+    }
 }
