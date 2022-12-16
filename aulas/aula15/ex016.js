c84241828acbe20a1d61c34287fb80c7d288edf6
David Moura <119620606+DavidMouraa@users.noreply.github.com>
2022-12-16 14:43:06 -0300

    Update ex016.js

--- aulas/aula15/ex016.js
+++ aulas/aula15/ex016.js
@@ -1,6 +1,13 @@
 let val = [5,3,9,5,7,8];
 
+/*
 for (let i = 0; i < val.length; i++)
+{
+    console.log(`A posição ${i} contém o valor: ${val[i]}`);
+}
+*/
+
+for (let i in val)
 {
     console.log(`A posição ${i} contém o valor: ${val[i]}`);
 }
